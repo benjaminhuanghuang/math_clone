7c237d97744f7f6f0b9b5de90918e8c144f17903
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-06-17 09:40:31 -0700

    add TODO

---
+++ TODO.md
@@ -0,0 +1,5 @@
+Math Clone
+================
+- Normal user can assess admin page by inputting URL of admin page
+- Involve Angular JS
+
--- app/management/routes.py
+++ app/management/routes.py
@@ -1,15 +1,18 @@
 from flask import render_template, request, redirect, url_for , current_app
+from flask_login import login_required
 
 from . import management
 from app.models.user import User
 
 @management.route('/usermanagement', )
+@login_required
 def usermanagement():
     users = User.get_all_users()
     return render_template('management/userlist.html', users = users)
 
 
 @management.route('/userdetail')
+@login_required
 def user_detail():
     return render_template('management/user_detail.html')
 
