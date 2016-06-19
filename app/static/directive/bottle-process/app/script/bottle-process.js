8b3f8357eef6045e0c610e17a710eddf4a437e0a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-06-18 19:14:02 -0700

    fix bottle-process

--- app/static/directive/bottle-process/app/script/bottle-process.js
+++ app/static/directive/bottle-process/app/script/bottle-process.js
@@ -1,10 +1,9 @@
-angular.module('bottleProcess',[])
-    .controller('MainCtrl',['$scope',function($scope){
-
+angular.module('bottleProcess', [])
+    .controller('MainCtrl', ['$scope', function ($scope) {
     }])
-    .directive('bottleProcess',function(){
-        function link(scope,element,attrs){
-            var maxWaterHeight = (attrs.per)*400;
+    .directive('bottleProcess', function () {
+        function link(scope, element, attrs) {
+            var maxWaterHeight = (attrs.per) * 400;
             var waterHeight = 0.6; // from 0 - 1.0
 
             var waterLevelTop = element.find('.waterLevelTop');
@@ -17,29 +16,29 @@ angular.module('bottleProcess',[])
             waterLevelTop.css({"height": "-10px"});
             waterLevel.css({"bottom": "-10px"});
 
-                waterLevelTop.animate({
-                        "height": stopPosition + "px"
-                    },
-                    {
-                        duration: 3000,
-                        easing: "easeOutCubic",
-                        queue: false
-                    });
+            waterLevelTop.animate({
+                    "height": stopPosition + "px"
+                },
+                {
+                    duration: 3000,
+                    easing: "easeOutCubic",
+                    queue: false
+                });
 
-                waterLevel.animate({
-                        "bottom": (stopPosition - 10) + "px"
-                    },
-                    {
-                        duration: 3000,
-                        easing: "easeOutCubic",
-                        queue: false
-                    });
+            waterLevel.animate({
+                    "bottom": (stopPosition - 10) + "px"
+                },
+                {
+                    duration: 3000,
+                    easing: "easeOutCubic",
+                    queue: false
+                });
         }
-        return{
-            strict:'E',
-            scope:{},
-            templateUrl: 'static/js/directive/bottle-process/app/bottle-process.html',
-            link:link
 
+        return {
+            strict: 'E',
+            scope: {},
+            templateUrl: '/static/directive/bottle-process/app/bottle-process.html',
+            link: link
         }
     })
